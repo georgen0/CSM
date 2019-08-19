d9626af9cd51e756d45237701dd2988a33776c14
georgen2 <negraiageorge@yahoo.ro>
2019-08-19 16:11:46 +0300

    Dynamic navbar

--- includes/Connection.php
+++ includes/Connection.php
@@ -3,10 +3,5 @@ function connect()
 {
     global $connection;
     $connection = mysqli_connect('localhost', 'root', '', 'cms');
-    if ($connection) {
-        echo "Success";
-    } else {
-        echo "Fail";
-    }
 }
 ?>
